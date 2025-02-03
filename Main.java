fb125dccc87c8841cc9b291797f26a6334034789
Hari-Sri-T <harisrishanmukhasai2005@gmail.com>
2025-02-03 10:40:51 +0530

    Added new class Main

---
+++ Main.java
@@ -0,0 +1,16 @@
+// Main.java
+
+import java.util.*;
+class Main{
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Name: ");
+		String name = scan.nextLine();
+		System.out.println("Enter PRN: ");
+		long prn = long.parseLong(scan.nextLine());
+		StudentOperations operations = new StudentOperations();
+		Student student = new Student(name,prn);
+		operations.addStudent(student);
+		operations.displayStudents();
+	}
+}
