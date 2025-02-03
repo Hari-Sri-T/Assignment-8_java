7c03785e4216987b0d3ff426647ce6965d765b8c
Hari-Sri-T <harisrishanmukhasai2005@gmail.com>
2025-02-03 10:09:45 +0530

    Created Student class with set and get methods

---
+++ Student.java
@@ -0,0 +1,14 @@
+//Student.java
+
+class Student{
+	private String name;
+	private long prn;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+}
